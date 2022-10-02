0fc44937882530f26369c46a50688497e80396c9
sakif hridoy <silenticebreaker@gmail.com>
2022-10-03 01:56:22 +0600

    apply conditions for sliding

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const slideshowElements = document.querySelectorAll(".slideshow-element");
 
 // console.log(slideshowElements)
-
+let  countElements = 1;
 setInterval(()=>{
-    let currentElement = document.querySelector(".current")
+    countElements ++;
+    let currentElement = document.querySelector(".current");
+    currentElement.classList.remove("current");
+
+    if (countElements > slideshowElements.length){
+        slideshowElements[0].classList.add("current")
+    }else{
+        currentElement.nextElementSibling.classList.add("current");
+    }
+
 },2000)
