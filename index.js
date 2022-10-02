fa5d52d3d7f5859672e0551457d1583db01429bb
sakif hridoy <silenticebreaker@gmail.com>
2022-10-03 01:36:09 +0600

    show items to console

---
+++ index.js
@@ -0,0 +1,7 @@
+const slideshowElements = document.querySelectorAll(".slideshow-element");
+
+// console.log(slideshowElements)
+
+setInterval(()=>{
+    let currentElement = document.querySelector(".current")
+},2000)
